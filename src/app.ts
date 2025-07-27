30c3ef65c6d8a35fe7901320b6b5759b8e5d913c
稳稳 <wenwen@wenwen.local>
2025-07-27 11:30:53 +0800

    fix

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,22 @@ function App({ children }: PropsWithChildren<any>) {
     // 使用callContainer前一定要init一下，全局执行一次即可
     wx.cloud.init();
     console.log("App launched.");
+    wx.cloud.callContainer({
+      config: {
+        env: "prod-4gcsgqa75da26b30",
+      },
+      path: "/",
+      header: {
+        "X-WX-SERVICE": "koa-s36g",
+      },
+      method: "GET",
+      success: (res) => {
+        console.log("success", res);
+      },
+      fail: (err) => {
+        console.log("fail", err);
+      },
+    } as any);
   });
 
   // children 是将要会渲染的页面
